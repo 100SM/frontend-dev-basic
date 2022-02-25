e31100a314edec505e825dfde7900c8dd192aecb
100SM <bsm2716@gmaile.com>
2022-02-25 16:16:32 +0900

    ajax #2

---
+++ ch08/src/main/java/com/poscoict/ch08/controller/test/ApiController.java
@@ -0,0 +1,19 @@
+package com.poscoict.ch08.controller.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller("apiTestController")
+@RequestMapping("/test")
+public class ApiController {
+
+	@RequestMapping("/text")
+	public String Text() {
+		return "text";
+	}
+
+	@RequestMapping("/html")
+	public String html() {
+		return "html";
+	}
+}
