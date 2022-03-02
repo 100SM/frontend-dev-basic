2f14b3e7e1080a2d002493fe7a57fd0b8ab0492a
100SM <bsm2716@gmaile.com>
2022-03-02 15:06:39 +0900

    guestbook spa #2

--- ch08/src/main/java/com/poscoict/ch08/controller/api/GuestbookController.java
+++ ch08/src/main/java/com/poscoict/ch08/controller/api/GuestbookController.java
@@ -1,9 +1,13 @@
 package com.poscoict.ch08.controller.api;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.poscoict.ch08.controller.dto.JsonResult;
@@ -21,5 +25,33 @@ public class GuestbookController {
 		vo.setPassword("");
 
 		return JsonResult.success(vo);
+	}
+
+	@ResponseBody
+	@RequestMapping(value = "/list", method = RequestMethod.POST)
+	public JsonResult ex02(@RequestParam(value = "sn", required = true, defaultValue = "-1") Long no) {
+		// guestbookService.getMessageList(no);
+
+		List<GuestbookVo> list = new ArrayList<GuestbookVo>();
+
+		GuestbookVo vo1 = new GuestbookVo();
+		vo1.setNo(1L);
+		vo1.setName("둘리1");
+		vo1.setPassword("안녕1");
+		list.add(vo1);
+
+		GuestbookVo vo2 = new GuestbookVo();
+		vo2.setNo(2L);
+		vo2.setName("둘리2");
+		vo2.setPassword("안녕2");
+		list.add(vo2);
+
+		GuestbookVo vo3 = new GuestbookVo();
+		vo3.setNo(3L);
+		vo3.setName("둘리3");
+		vo3.setPassword("안녕3");
+		list.add(vo3);
+
+		return JsonResult.success(list);
 	}
 }
