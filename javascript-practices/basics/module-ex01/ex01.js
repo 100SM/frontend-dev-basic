55e228d7e22b60794d12ab1b7154038a83be5b58
100SM <bsm2716@gmaile.com>
2022-03-03 15:24:56 +0900

    require 함수 사용법

---
+++ javascript-practices/basics/module-ex01/ex01.js
@@ -0,0 +1,12 @@
+/**
+ * 분리되지 app
+ */
+
+var app = function () {
+  var app = {};
+  app.textContents = "Hello World";
+
+  return app;
+};
+
+console.log("app() :>> ", app());
---
+++ javascript-practices/basics/module-ex04/error-module03.js
@@ -0,0 +1,9 @@
+module.exports = {
+  n: 10,
+  s: "javascript module",
+  o: {},
+};
+
+// 얘네 둘은 안됨
+exports.b = false;
+exports.f = function () {};
